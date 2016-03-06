f5521437d46266d4e8c14dafe4079f7cd04ab0a7
jj3274 <jaewook77@gmail.com>
2016-03-05 23:50:05 -0800

    Completed

--- js/app.js
+++ js/app.js
@@ -33,69 +33,72 @@ Once you have completed implementing the Player and Enemy, you should instantiat
 */
 
 // Customizable Constants for Game Setting
-var ENEMY_MIN_SPEED = 2,    // Enemy Bug's min speed
-    ENEMY_MAX_SPEED = 6;    // Enemy Bug's max speed
-var SCORE_POINT_FOR_REACH_TO_WATER = 10,  // score collection point when a player reaches to water
-    SCORE_POINT_FOR_GEM_COLLECTION = 2;  // score collection point when a player collects a gem
-var MIN_NUM_OF_GEMS = 2,  // minimum number of gem in the game
-    MAX_NUM_OF_GEMS = 8;  // maximum number of gem in the game
+var ENEMY_MIN_SPEED = 2, // Enemy Bug's min speed
+  ENEMY_MAX_SPEED = 6; // Enemy Bug's max speed
+var SCORE_POINT_FOR_REACH_TO_WATER = 10, // score collection point when a player reaches to water
+  SCORE_POINT_FOR_GEM_COLLECTION = 2; // score collection point when a player collects a gem
+var MIN_NUM_OF_GEMS = 2, // minimum number of gem in the game
+  MAX_NUM_OF_GEMS = 8; // maximum number of gem in the game
 var NUM_OF_ENEMIES = 3; // number of enemies that will appear at the same time
-var COLLIDE_CENTER_OFFSET = 20;       // Enemy and Player's Center Hit Offset,
+var COLLIDE_CENTER_OFFSET = 20; // Enemy and Player's Center Hit Offset,
 // e.g., if COLLIDE_CENTER_OFFSET = 0, then Enemy's center should exactly match to Player's center coordinate to collide
 
 // Constants Variables
 var TILE_HEIGHT = 83; // Tile's Height (Col)
 var TILE_WIDTH = 101; // Tile's Width (Row)
-var NUM_OF_ROWS = 6,     // Num of Rows
-    NUM_OF_COLS = 5;     // Num of Cols
-var ENEMY_Y_OFFSET = -20,   // Enemy Bug's Y-coordinate offset for display
-    PLAYER_Y_OFFSET = -10;  // Player's Y-coordinate offset for display
+var NUM_OF_ROWS = 6, // Num of Rows
+  NUM_OF_COLS = 5; // Num of Cols
+var ENEMY_Y_OFFSET = -20, // Enemy Bug's Y-coordinate offset for display
+  PLAYER_Y_OFFSET = -10; // Player's Y-coordinate offset for display
 
 var PLAYER_IMAGE_SET = [
-         'images/char-boy.png',
-         'images/char-cat-girl.png',
-         'images/char-horn-girl.png',
-         'images/char-pink-girl.png',
-         'images/char-princess-girl.png'
-     ];
+  'images/char-boy.png',
+  'images/char-cat-girl.png',
+  'images/char-horn-girl.png',
+  'images/char-pink-girl.png',
+  'images/char-princess-girl.png'
+];
 var GEM_IMAGE_SET = [
-        'images/Gem Blue.png',
-        'images/Gem Green.png',
-        'images/Gem Orange.png'
-    ];
+  'images/Gem Blue.png',
+  'images/Gem Green.png',
+  'images/Gem Orange.png'
+];
 
-// Enemies our player must avoid
+/**
+ * Enemies our player must avoid
+ */
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.initNewEnemy();
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
+  this.initNewEnemy();
 };
 
 Enemy.prototype.initNewEnemy = function() {
-    this.col = -1;
-    this.row = Math.floor((Math.random() * 3) + 1) // random number: 1, 2, or 3
-    this.speed = Math.floor((Math.random() * (ENEMY_MAX_SPEED - ENEMY_MIN_SPEED)) + ENEMY_MIN_SPEED); // speed : 3 ~ 6 col moves / sec
+  this.col = -1;
+  this.row = Math.floor((Math.random() * 3) + 1) // random number: 1, 2, or 3
+  this.speed = Math.floor((Math.random() * (ENEMY_MAX_SPEED - ENEMY_MIN_SPEED)) +
+    ENEMY_MIN_SPEED); // speed : 3 ~ 6 col moves / sec
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.col += dt * this.speed;
-    if (this.col > NUM_OF_ROWS) {
-      this.initNewEnemy();
-    }
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.col += dt * this.speed;
+  if (this.col > NUM_OF_ROWS) {
+    this.initNewEnemy();
+  }
 
-    if (this.isCollide()) { // Enemy caught the player, reset game
-      player.score = 0;
-      gameController.init();
-    }
+  if (this.isCollide()) { // Enemy caught the player, reset game
+    player.score = 0;
+    gameController.init();
+  }
 };
 
 Enemy.prototype.x = function() {
@@ -108,7 +111,7 @@ Enemy.prototype.y = function() {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x(), this.y());
+  ctx.drawImage(Resources.get(this.sprite), this.x(), this.y());
 };
 
 // Draw the enemy on the screen, required method for game
@@ -158,12 +161,12 @@ Player.prototype.render = function() {
   // draw player character
   ctx.drawImage(Resources.get(this.sprite), this.x(), this.y());
 
-  ctx.font="20px Georgia";
-  ctx.clearRect(0, 0, 505, 50);  // clear score board and player icon panel
+  ctx.font = "20px Georgia";
+  ctx.clearRect(0, 0, 505, 50); // clear score board and player icon panel
 
   // draw player icons
   for (var i = 0; i < PLAYER_IMAGE_SET.length; i++) {
-    ctx.fillText((i+1), i * 50 + 120, 35);
+    ctx.fillText((i + 1), i * 50 + 120, 35);
     ctx.drawImage(Resources.get(PLAYER_IMAGE_SET[i]), i * 50 + 130, 0, 30, 50);
   }
 
@@ -189,8 +192,7 @@ Player.prototype.handleInput = function(key) {
         this.score += SCORE_POINT_FOR_REACH_TO_WATER; // you succeed, gain score
         if (this.highest < this.score) this.highest = this.score;
         gameController.init();
-      }
-      else { // otherwise, player can move up
+      } else { // otherwise, player can move up
         this.row--;
       }
       break;
@@ -201,19 +203,19 @@ Player.prototype.handleInput = function(key) {
       break;
 
     default:
-     if ('1' <= key && key <= '5') {
-       var playerImageIndex = key - '1';
-       this.sprite = PLAYER_IMAGE_SET[playerImageIndex];
-     }
+      if ('1' <= key && key <= '5') {
+        var playerImageIndex = key - '1';
+        this.sprite = PLAYER_IMAGE_SET[playerImageIndex];
+      }
 
-     return;
+      return;
   }
 
   // Player moved to new tile, so do something for this movement
   gameController.playerMoved();
 };
 
-// This is Gem class
+// This is Gem tile class
 var Gem = function() {
   var gemImageIndex = Math.floor(Math.random() * GEM_IMAGE_SET.length) // random number: 1, 2, or 3
 
@@ -232,9 +234,9 @@ Gem.prototype.y = function() {
 };
 
 Gem.prototype.render = function() {
-    if (this.active) {
-      ctx.drawImage(Resources.get(this.sprite), this.x(), this.y(), 30, 50);
-    }
+  if (this.active) {
+    ctx.drawImage(Resources.get(this.sprite), this.x(), this.y(), 30, 50);
+  }
 };
 
 // This is Game Controller class
@@ -247,7 +249,8 @@ GameController.prototype.init = function() {
   player.initPlayer();
 
   // Init Gem tiles
-  numOfGems = Math.floor((Math.random() * (MAX_NUM_OF_GEMS - MIN_NUM_OF_GEMS + 1)) + MIN_NUM_OF_GEMS);
+  numOfGems = Math.floor((Math.random() * (MAX_NUM_OF_GEMS - MIN_NUM_OF_GEMS +
+    1)) + MIN_NUM_OF_GEMS);
   allGems = [];
   for (var i = 0; i < numOfGems; i++) {
     var gem = new Gem();
@@ -255,6 +258,9 @@ GameController.prototype.init = function() {
   }
 };
 
+/**
+ * when a player moves, check if there is a gem that the player can take.
+ */
 GameController.prototype.playerMoved = function() {
   for (var i = 0; i < allGems.length; i++) {
     if (player.col === allGems[i].col && player.row === allGems[i].row) {
@@ -287,17 +293,17 @@ var gameController = new GameController();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down',
-        49: '1',
-        50: '2',
-        51: '3',
-        52: '4',
-        53: '5'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down',
+    49: '1',
+    50: '2',
+    51: '3',
+    52: '4',
+    53: '5'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
